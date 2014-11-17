23414cd1351a549d60c516014c325547397c8cef
CoopRepository <brendan.kellam@greenwoodcollege.com>
2014-11-17 03:14:15 -0800

    commented info with bug fixes

--- Src/pieces/Queen.rb
+++ Src/pieces/Queen.rb
@@ -1,12 +1,12 @@
 require "./pieces/Piece"
 
-class Queen < Piece
+class Queen < Piece #saying queen is a piece
 
-	def initialize(x, y)
-		set_id(6);
-		set_disp("q");
-		@x = x;
-		@y = y;
+	def initialize(x, y) #what the parameters of the queen is 
+		set_id(5); #setting id to 5
+		set_disp("q"); #setting display to q
+		@x = x; #setting x coordinate
+		@y = y; #setting y coordinate
 	end
 
 	def add(a, b)
