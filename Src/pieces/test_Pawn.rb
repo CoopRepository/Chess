dc2f6a484a0416e37b23f81e3f25a42610508dbf
CoopRepository <brendan.kellam@greenwoodcollege.com>
2014-11-24 16:25:49 -0500

    made changes to pawn unit test

--- Src/pieces/test_Pawn.rb
+++ Src/pieces/test_Pawn.rb
@@ -108,7 +108,7 @@ class Test_Pawn < Minitest::Test
 	def test_linear_move_team_blue
 		
 		pawn = @board.get_piece(@bt_x, @bt_y)
-		pawn_enemy = board.get_piece(@rt_x,@rt_y)
+		pawn_enemy = @board.get_piece(@rt_x,@rt_y)
 		pawn_enemy.move(0,2)
 		pawn_enemy.move(0,3)
 		pawn_enemy.move(0,4)
@@ -121,7 +121,7 @@ class Test_Pawn < Minitest::Test
 	
 	def test_horizontal_move_team_blue
 	
-		pawn = board.get_piece(@bt_x,@bt_y)
+		pawn = @board.get_piece(@bt_x,@bt_y)
 		pawn.move(0,5)
 		result = pawn.move(1, 5)
 		assert_equal false, result
