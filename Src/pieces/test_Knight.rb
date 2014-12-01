9a0298dcd04bac4bca3c1246dfdea89c2b4d21b1
CoopRepository <brendan.kellam@greenwoodcollege.com>
2014-12-01 04:02:01 -0800

    test better

--- Src/pieces/test_Knight.rb
+++ Src/pieces/test_Knight.rb
@@ -12,35 +12,35 @@ class Test_Knight < Minitest::Test
 	end
 
 	def test_vertical_incorrect_linear_movement
-		Knight = @board.get_piece(@x, @y);
-		assert !Knight.move(2, 1);
+		knight = @board.get_piece(@x, @y);
+		assert !knight.move(2, 1);
 	end
 	
 	def test_horzintal_incorrect_linear_movement
-		Knight = @board.get_piece(@x, @y);
-		assert !Knight.move(0, 3);
-	end
-	
+		knight = @board.get_piece(@x, @y);
+		assert !knight.move(0, 3);
 	def test_incorrect_diagonal_movement
-		Knight = @board.get_piece(@x, @y);
-		assert !Knight.move(2, 1);
+		knight = @board.get_piece(@x, @y);
+		assert !knight.move(2, 1);
 	end
 	
 	def test_colliding_friendly
-		Knight = @board.get_piece(@x, @y);
-		assert !Knight.move(3, 1);
+		knight = @board.get_piece(@x, @y);
+		assert !knight.move(3, 1);
+	end
 	end
 	
+	
 	def test_correct_movement_nocapture
-		Knight = @board.get_piece(@x, @y);
-		assert !Knight.move(2, 2);
+		knight = @board.get_piece(@x, @y);
+		assert knight.move(2, 2);
 	end
 	def test_correct_movement_capture
-		Knight = @board.get_piece(@x, @y);
-		Knight.move(2,2)
-		Knight.move(3,4)
+		knight = @board.get_piece(@x, @y);
+		knight.move(2,2)
+		knight.move(3,4)
 		assert knight.move(4,6)
 	end
 	
 		
-		
+end
