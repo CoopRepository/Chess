08f165c19c78770546780650789360af63c15853
CoopRepository <brendan.kellam@greenwoodcollege.com>
2014-12-08 13:50:39 -0500

    Worked more on unit testing

--- Src/pieces/Bishop.rb
+++ Src/pieces/Bishop.rb
@@ -15,10 +15,24 @@ class Bishop < Piece #inherits from Piece.rb
 	
 	def move(x, y)
 		
+		@xdist = @x - x;
+		@ydist = @y - y;
 		
+		if !check_diagonal_move 
+			return false
+		end
 		
+		@x = x;
+		@y = y;
 	end
 	
-
+	def check_diagonal_move
+		if(@xdist.abs > 0 and @ydist.abs > 0) 
+			return true
+		end
+		
+		puts("cannot move the bishop non-diagonally")
+		return false
+	end
 	
 end
